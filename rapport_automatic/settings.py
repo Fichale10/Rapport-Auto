3201015a55ee5fda8d6942f6e0e0823ff44a4aa1
Fichale10 <fichaleabdoul@gmail.com>
2026-04-28 05:36:19 +0000

    MAJ

--- rapport_automatic/settings.py
+++ rapport_automatic/settings.py
@@ -10,8 +10,8 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY',
     'django-insecure-fallback-key-change-in-prod'
 )
-
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
